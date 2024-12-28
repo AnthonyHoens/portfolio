83567566220b77d09f88e53c85b1774fefbccaa4
Anthony Hoens <anthony.hoens@hotmail.com>
2024-12-28 19:18:40 +0100

    Add Contact form 7

--- src/bedrock/web/app/themes/portfolio/contact.php
+++ src/bedrock/web/app/themes/portfolio/contact.php
@@ -6,5 +6,5 @@
         <?= esc_html__('Get in touch', THEME_TEXT_DOMAIN) ?>
     </h2>
 
-    <?= do_shortcode( '[contact-form-7 id="65" title="Contact"]' ); ?>
+    <?= do_shortcode( '[contact-form-7 id="fb60c37" title="Formulaire de contact 1"]' ); ?>
 </section>
