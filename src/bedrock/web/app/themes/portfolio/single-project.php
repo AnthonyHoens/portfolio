f4ae64fab563f33abbc64c04b482c012133a9cb7
Anthony Hoens <anthony.hoens@hotmail.com>
2024-12-29 15:28:28 +0100

    Fix single project

--- src/bedrock/web/app/themes/portfolio/single-project.php
+++ src/bedrock/web/app/themes/portfolio/single-project.php
@@ -26,50 +26,7 @@
                 </a>
             </div>
         </div>
-
-        <section class="single_project__support">
-            <h3 role="heading" aria-level="3" class="sro">
-                <?php the_title(); ?> sur différents support.
-            </h3>
-            <div class="single_project__support_img_container">
-                <img <?= dw_the_img_attributes(get_field('support_img'), ['thumbnail','medium', 'large']) ?>>
-            </div>
-            <p class="single_project__support__text">
-                <?php the_field('support_text'); ?>
-            </p>
-        </section>
-
-        <section class="single_project__moodboard moodboard">
-            <h3 aria-level="3" role="heading" class="moodboard__title">
-                Moodboard
-                <span></span>
-            </h3>
-            <div class="moodboard__container">
-                <div class="moodboard__img_container">
-                    <img <?= dw_the_img_attributes(get_field('moodboard_img'), ['thumbnail','medium', 'large']) ?>>
-                </div>
-                <p class="moodboard__text">
-                    <?php the_field('moodboard_text'); ?>
-                </p>
-            </div>
-        </section>
-
-        <section class="single_project__design design">
-            <h3 aria-level="3" role="heading" class="design__title">
-                Design
-                <span></span>
-            </h3>
-            <div class="design__container">
-                <div class="design__computer_design">
-                    <img <?= dw_the_img_attributes(get_field('design_img_computer'), ['thumbnail','medium', 'large']) ?>>
-                </div>
-                <div class="design__phone_design">
-                    <img <?= dw_the_img_attributes(get_field('design_img_phone'), ['medium', 'large']) ?>>
-                </div>
-            </div>
-        </section>
     </section>
-    <?php include 'contact.php'; ?>
 </main>
 
 <?php get_footer(); ?>
