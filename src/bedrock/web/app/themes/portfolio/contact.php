ebfcf8f4e6763cfd31886dfb537628ed112251b3
Anthony Hoens <anthony.hoens@hotmail.com>
2024-12-30 14:28:42 +0100

    Add translations and fix arrows

--- src/bedrock/web/app/themes/portfolio/contact.php
+++ src/bedrock/web/app/themes/portfolio/contact.php
@@ -5,6 +5,4 @@
     <h2 class="contact__title" role="heading" aria-level="2">
         <?= esc_html__('Get in touch', THEME_TEXT_DOMAIN) ?>
     </h2>
-
-    <?= do_shortcode( '[contact-form-7 id="fb60c37" title="Formulaire de contact 1"]' ); ?>
 </section>
--- src/bedrock/web/app/themes/portfolio/index.php
+++ src/bedrock/web/app/themes/portfolio/index.php
@@ -9,6 +9,9 @@
             <a href="<?= esc_url(sprintf('mailto:%s', get_option('new_admin_email'))) ?>" class="btn btn--primary" itemprop="url">
                 <?= esc_html__('Get in touch', THEME_TEXT_DOMAIN) ?>
             </a>
+            <div class="landing--arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46"><g fill="none" fill-rule="evenodd"><circle cx="23" cy="23" r="23" fill="#1B8284"/><path fill="#FFF" fill-rule="nonzero" d="m22.994 26.628 7.741-7.746c.264-.263.575-.39.935-.382.36.01.672.145.935.409.263.263.395.575.395.935 0 .36-.132.672-.395.935l-8.11 8.089c-.211.21-.448.369-.711.474a2.112 2.112 0 0 1-.79.158c-.264 0-.527-.053-.79-.158a2.104 2.104 0 0 1-.711-.474l-8.11-8.115a1.218 1.218 0 0 1-.383-.922c.01-.351.145-.659.409-.922.263-.264.575-.395.934-.395.36 0 .672.131.935.395l7.716 7.72Z"/></g></svg>
+            </div>
         </section>
 
         <section class="projects-list" id="projects">
